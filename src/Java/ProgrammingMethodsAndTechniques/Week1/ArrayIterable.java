503c6fe2307c3c69f1dd48602ec2b208ea1c9a50
asuvorkov <andrey.suvorkov@gmail.com>
2018-04-17 18:02:12 +0200

    Programming methods and techniques - Week 1 - ArrayIterable

---
+++ src/Java/ProgrammingMethodsAndTechniques/Week1/ArrayIterable.java
@@ -0,0 +1,46 @@
+package Java.ProgrammingMethodsAndTechniques.Week1;
+
+import java.util.Iterator;
+
+/**
+ * Created by Andrei on 17.04.2018.
+ */
+public class ArrayIterable<A> implements Iterable<A> {
+  A[] as;
+
+  ArrayIterable(A[] as){
+    this.as = as;
+  }
+
+  public static void main(String[] args) {
+    String[] as = {"one", "two", "three"};
+    Iterator<String> it = new ArrayIterable<>(as).iterator();
+    for ( ; it.hasNext(); ){
+      System.out.println(it.next());
+    }
+  }
+
+  @Override
+  public Iterator<A> iterator() {
+    return new Iterable(as);
+  }
+
+  private class Iterable implements Iterator<A>{
+    int currentIndex = 0;
+    A[] as;
+
+    Iterable(A[] as) {
+      this.as = as;
+    }
+
+    @Override
+    public boolean hasNext() {
+      return currentIndex < as.length;
+    }
+
+    @Override
+    public A next() {
+      return as[currentIndex++];
+    }
+  }
+}
