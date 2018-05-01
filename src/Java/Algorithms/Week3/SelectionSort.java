10227a88f6902740c14b11a817bd46b7019aae85
asuvorkov <andrey.suvorkov@gmail.com>
2018-05-01 14:28:22 +0200

    Algorithms - Week3 - SelectionSort

---
+++ src/Java/Algorithms/Week3/SelectionSort.java
@@ -0,0 +1,28 @@
+package Java.Algorithms.Week3;
+
+/**
+ * Created by Andrei on 01.05.2018.
+ */
+public class SelectionSort extends Sort{
+  public void sort(int[] a) {
+    // FIXME: implement
+    for (int i = 0; lt(i, a.length - 1); i++){
+      int index = i;
+      for (int j = i + 1; lt(j, a.length); j++) {
+        if (gt(a[j], a[index])) {
+          index = j;
+        }
+      }
+      swap(a, index, i);
+    }
+  }
+
+  public static void main(String[] args) {
+    SelectionSort sort = new SelectionSort();
+    sort.runSmall(10000);
+
+    /*
+    Array n length has -> n^2 compares and n^2 changes
+     */
+  }
+}
